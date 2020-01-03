4fd4d786ba19a15e39dd6bb1f4f10e4855cd0c7a
Damian Lewis <damian@damianlewis.co.uk>
2020-01-03 17:36:59 +0000

    Validate preview image settings

--- plugins/damianlewis/customcolumns/models/Settings.php
+++ plugins/damianlewis/customcolumns/models/Settings.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace DamianLewis\CustomColumns\Models;
 
 use Model;
+use October\Rain\Database\Traits\Validation;
 use System\Behaviors\SettingsModel;
 
 class Settings extends Model
 {
+    use Validation;
+
     public $implement = [
         SettingsModel::class
     ];
@@ -16,4 +19,9 @@ class Settings extends Model
     public $settingsCode = 'damianlewis_custom_columns_settings';
 
     public $settingsFields = 'fields.yaml';
+
+    public $rules = [
+        'preview_image_width' => 'integer',
+        'preview_image_height' => 'integer'
+    ];
 }
