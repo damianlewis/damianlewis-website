4de2af198184eee48734d01aa068e20332e88c14
Damian Lewis <damian@damianlewis.co.uk>
2020-01-10 12:39:19 +0000

    Added accordion list vue component

---
+++ themes/damianlewis/src/js/components/AccordionList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="">
+        <div class="flex items-center justify-between">
+            <span class="c-text text-grey-600 cursor-pointer" @click="isOpen = !isOpen">{{ title }}</span>
+            <button class="h-6" @click="isOpen = !isOpen">
+                <img v-if="isOpen" :src="closeButton" class="h-full" alt="Close icon">
+                <img v-else :src="openButton" class="h-full" alt="Open icon">
+            </button>
+        </div>
+        <div class="overflow-hidden" :class="{'accordion--closed': !isOpen, 'accordion--open': isOpen}">
+            <div v-for="item in list" class="[ c-text c-text--small ] text-grey-600">{{ item.name }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'AccordionList',
+
+    props: {
+      title: String,
+      list: Array,
+      openButton: String,
+      closeButton: String,
+    },
+
+    data () {
+      return {
+        isOpen: false,
+      }
+    },
+  }
+</script>
+
+<style scoped>
+    .accordion--open {
+        max-height: 500px ;
+        transition: max-height 0.4s ease-in;
+    }
+
+    .accordion--closed {
+        max-height: 0;
+        transition: max-height 0.4s ease-out;
+    }
+</style>
--- themes/damianlewis/src/js/main.js
+++ themes/damianlewis/src/js/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Navigation from './components/Navigation'
 import NavigationButton from './components/NavigationButton'
+import AccordionList from './components/AccordionList'
 
 Vue.component('navigation', Navigation)
 Vue.component('navigation-button', NavigationButton)
+Vue.component('accordion-list', AccordionList)
 
 new Vue({
     el: '#page',
