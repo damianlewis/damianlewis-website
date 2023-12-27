3b0e5965ea928035b48037f5bf68975c5894f6a3
Damian Lewis <damian@damianlewis.co.uk>
2023-12-27 12:07:33 +0000

    feat: add deleted at placeholder form component

---
+++ app/Filament/Forms/Components/DeletedAtPlaceholder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Forms\Components;
+
+class DeletedAtPlaceholder extends DateTimePlaceholder
+{
+    public static function make(
+        ?string $name = 'deleted_at',
+    ): static {
+        return parent::make($name)
+            ->label('Deleted');
+    }
+}
