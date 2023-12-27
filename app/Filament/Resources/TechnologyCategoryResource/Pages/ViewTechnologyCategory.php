1f7daf7c293bab1c991c108eeb65c968f0fb330d
Damian Lewis <damian@damianlewis.co.uk>
2023-12-27 17:47:49 +0000

    feat: add edit button to view pages

--- app/Filament/Resources/TechnologyCategoryResource/Pages/ViewTechnologyCategory.php
+++ app/Filament/Resources/TechnologyCategoryResource/Pages/ViewTechnologyCategory.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\TechnologyCategoryResource\Pages;
 
 use App\Filament\Resources\TechnologyCategoryResource;
+use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewTechnologyCategory extends ViewRecord
 {
     protected static string $resource = TechnologyCategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            EditAction::make(),
+        ];
+    }
 }
--- app/Filament/Resources/TechnologyResource/Pages/ViewTechnology.php
+++ app/Filament/Resources/TechnologyResource/Pages/ViewTechnology.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\TechnologyResource\Pages;
 
 use App\Filament\Resources\TechnologyResource;
+use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewTechnology extends ViewRecord
 {
     protected static string $resource = TechnologyResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            EditAction::make(),
+        ];
+    }
 }
