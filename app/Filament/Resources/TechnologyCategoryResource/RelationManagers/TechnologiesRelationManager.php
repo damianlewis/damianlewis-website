54f96d5464823d5359cecba404e79d9c1f67e17b
Damian Lewis <damian@damianlewis.co.uk>
2023-12-28 12:15:13 +0000

    refactor: use technology resource form for technology relation manager

--- app/Filament/Resources/TechnologyCategoryResource/RelationManagers/TechnologiesRelationManager.php
+++ app/Filament/Resources/TechnologyCategoryResource/RelationManagers/TechnologiesRelationManager.php
@@ -2,30 +2,19 @@
 
 namespace App\Filament\Resources\TechnologyCategoryResource\RelationManagers;
 
-use App\Filament\Forms\Components\EnabledSection;
-use App\Filament\Forms\Components\TimestampsSection;
 use App\Filament\Resources\TechnologyResource;
 use App\Filament\Tables\Columns\CreatedAtTextColumn;
 use App\Filament\Tables\Columns\DeletedAtTextColumn;
 use App\Filament\Tables\Columns\EnabledIconColumn;
 use App\Filament\Tables\Columns\SortOrderTextColumn;
 use App\Filament\Tables\Columns\UpdatedAtTextColumn;
-use App\Models\Technology;
-use Closure;
 use Exception;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Group;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Infolists\Infolist;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables\Actions\CreateAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Validation\Rules\Exists;
 
 class TechnologiesRelationManager extends RelationManager
 {
@@ -38,63 +27,7 @@ class TechnologiesRelationManager extends RelationManager
 
     public function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Grid::make([
-                    'md' => 3,
-                ])
-                    ->schema([
-                        Group::make()
-                            ->schema([
-                                Section::make('Details')
-                                    ->schema([
-                                        TextInput::make('name')
-                                            ->autocapitalize('words')
-                                            ->required()
-                                            ->unique(ignoreRecord: true)
-                                            ->maxLength(255),
-                                    ]),
-                            ])
-                            ->columnSpan([
-                                'md' => 2,
-                            ]),
-                        Group::make()
-                            ->schema([
-                                TimestampsSection::make(),
-                                EnabledSection::make(),
-                                Section::make()
-                                    ->schema([
-                                        Select::make('parent_id')
-                                            ->relationship(
-                                                name: 'parent',
-                                                titleAttribute: 'name',
-                                                modifyQueryUsing: fn (Builder $query, ?Technology $record, RelationManager $livewire): Builder => $query
-                                                    ->whereNull('parent_id')
-                                                    ->whereNot((new Technology)->getKeyName(), $record?->getKey())
-                                                    ->where('technology_category_id', $livewire->getOwnerRecord()->getKey())
-                                            )
-                                            ->searchable()
-                                            ->preload()
-                                            ->nullable()
-                                            ->exists(
-                                                column: (new Technology)->getKeyName(),
-                                                modifyRuleUsing: fn (Exists $rule, ?Technology $record, RelationManager $livewire): Exists => $rule
-                                                    ->whereNull('parent_id')
-                                                    ->whereNot((new Technology)->getKeyName(), $record?->getKey())
-                                                    ->where('technology_category_id', $livewire->getOwnerRecord()->getKey())
-                                            )
-                                            ->rule(fn (?Technology $record): Closure => static fn (string $attribute, string $value, Closure $fail): ?string => $record?->hasChildren()
-                                                ? $fail($record->name . ' can\'t have a parent as it is already a parent')
-                                                : null
-                                            ),
-                                    ])
-                                    ->hidden(fn (?Technology $record) => $record?->hasChildren()),
-                            ])
-                            ->columnSpan([
-                                'md' => 1,
-                            ]),
-                    ]),
-            ]);
+        return TechnologyResource::form($form);
     }
 
     /**
