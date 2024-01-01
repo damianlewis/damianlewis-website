3cc268e32bf6fb9c6a8fba5c1f1576f474cca244
Damian Lewis <damian@damianlewis.co.uk>
2024-01-01 12:10:06 +0000

    refactor: increase modal width for technology relation manager actions

--- app/Filament/Resources/TechnologyCategoryResource/RelationManagers/TechnologiesRelationManager.php
+++ app/Filament/Resources/TechnologyCategoryResource/RelationManagers/TechnologiesRelationManager.php
@@ -8,11 +8,16 @@ use App\Filament\Tables\Columns\DeletedAtTextColumn;
 use App\Filament\Tables\Columns\EnabledIconColumn;
 use App\Filament\Tables\Columns\SortOrderTextColumn;
 use App\Filament\Tables\Columns\UpdatedAtTextColumn;
+use App\Models\Technology;
 use Exception;
 use Filament\Forms\Form;
 use Filament\Infolists\Infolist;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -54,8 +59,17 @@ class TechnologiesRelationManager extends RelationManager
                 UpdatedAtTextColumn::make(),
                 DeletedAtTextColumn::make(),
             ])
+            ->actions([
+                ViewAction::make()
+                    ->modalWidth(MaxWidth::FiveExtraLarge),
+                EditAction::make()
+                    ->modalWidth(MaxWidth::FiveExtraLarge)
+                    ->hidden(fn (Technology $record): bool => $record->trashed()),
+                DeleteAction::make(),
+            ])
             ->headerActions([
-                CreateAction::make(),
+                CreateAction::make()
+                    ->modalWidth(MaxWidth::FiveExtraLarge),
             ])
             ->reorderable(config('eloquent-sortable.order_column_name'))
             ->defaultSort(config('eloquent-sortable.order_column_name'));
