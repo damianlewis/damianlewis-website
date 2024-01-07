c80047d305f136fa77ee71f047fe3211ecfdb956
Damian Lewis <damian@damianlewis.co.uk>
2024-01-07 18:24:44 +0000

    refactor: extract edit record class

--- app/Filament/Resources/TechnologyCategoryResource/Pages/EditTechnologyCategory.php
+++ app/Filament/Resources/TechnologyCategoryResource/Pages/EditTechnologyCategory.php
@@ -2,20 +2,10 @@
 
 namespace App\Filament\Resources\TechnologyCategoryResource\Pages;
 
+use App\Filament\Resources\Pages\EditRecord;
 use App\Filament\Resources\TechnologyCategoryResource;
-use Filament\Actions\DeleteAction;
-use Filament\Actions\ViewAction;
-use Filament\Resources\Pages\EditRecord;
 
 class EditTechnologyCategory extends EditRecord
 {
     protected static string $resource = TechnologyCategoryResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            ViewAction::make(),
-            DeleteAction::make(),
-        ];
-    }
 }
