f8d2bf8898bea5521613543e212e36616f018722
Damian Lewis <damian@damianlewis.co.uk>
2024-01-07 18:25:09 +0000

    refactor: extract list records class

--- app/Filament/Resources/TechnologyResource/Pages/ListTechnologies.php
+++ app/Filament/Resources/TechnologyResource/Pages/ListTechnologies.php
@@ -2,18 +2,10 @@
 
 namespace App\Filament\Resources\TechnologyResource\Pages;
 
+use App\Filament\Resources\Pages\ListRecords;
 use App\Filament\Resources\TechnologyResource;
-use Filament\Actions\CreateAction;
-use Filament\Resources\Pages\ListRecords;
 
 class ListTechnologies extends ListRecords
 {
     protected static string $resource = TechnologyResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
 }
