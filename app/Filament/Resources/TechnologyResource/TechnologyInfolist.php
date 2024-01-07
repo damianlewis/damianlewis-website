94d46d24aac6badcfddd53d6d6b41f3bab694fba
Damian Lewis <damian@damianlewis.co.uk>
2024-01-07 18:46:21 +0000

    refactor: extract info list sections to separate methods

--- app/Filament/Resources/TechnologyResource/TechnologyInfolist.php
+++ app/Filament/Resources/TechnologyResource/TechnologyInfolist.php
@@ -18,29 +18,49 @@ class TechnologyInfolist
             ->schema([
                 Group::make()
                     ->schema([
-                        Section::make('Details')
-                            ->schema(self::getDetailsSchema()),
+                        self::getDetailsSection(),
                     ])
                     ->columnSpan(['lg' => 2]),
                 Group::make()
                     ->schema([
                         DatesSection::make(),
-                        Section::make('Category')
-                            ->schema(self::getCategorySchema())
-                            ->visible(
-                                fn (Technology $record): bool => $record->hasCategory()
-                            ),
-                        Section::make('Parent')
-                            ->schema(self::getParentSchema())
-                            ->visible(
-                                fn (Technology $record): bool => $record->hasParent()
-                            ),
-                        Section::make('Settings')
-                            ->schema(self::getSettingsSchema()),
+                        self::getCategorySection(),
+                        self::getParentSection(),
+                        self::getSettingsSection(),
                     ])
                     ->columnSpan(['lg' => 1]),
             ])
             ->columns(3);
+    }
+
+    public static function getDetailsSection(): Section
+    {
+        return Section::make('Details')
+            ->schema(self::getDetailsSchema());
+    }
+
+    public static function getCategorySection(): Section
+    {
+        return Section::make('Category')
+            ->schema(self::getCategorySchema())
+            ->visible(
+                fn (Technology $record): bool => $record->hasCategory()
+            );
+    }
+
+    public static function getParentSection(): Section
+    {
+        return Section::make('Parent')
+            ->schema(self::getParentSchema())
+            ->visible(
+                fn (Technology $record): bool => $record->hasParent()
+            );
+    }
+
+    public static function getSettingsSection(): Section
+    {
+        return Section::make('Settings')
+            ->schema(self::getSettingsSchema());
     }
 
     public static function getDetailsSchema(): array
