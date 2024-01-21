76e497dcc9e6ee383c78ba794c7a40ac12e3fc66
Damian Lewis <damian@damianlewis.co.uk>
2024-01-21 10:53:13 +0000

    refactor: use resource table class for technology relation manager table

--- app/Filament/Resources/TechnologyCategoryResource/RelationManagers/TechnologiesRelationManager.php
+++ app/Filament/Resources/TechnologyCategoryResource/RelationManagers/TechnologiesRelationManager.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\TechnologyCategoryResource\RelationManagers;
 
 use App\Filament\Resources\TechnologyResource;
+use App\Filament\Tables\Actions\DisableBulkAction;
+use App\Filament\Tables\Actions\EnableBulkAction;
 use App\Filament\Tables\Columns\CreatedAtTextColumn;
 use App\Filament\Tables\Columns\DeletedAtTextColumn;
 use App\Filament\Tables\Columns\EnabledIconColumn;
@@ -10,6 +12,8 @@ use App\Filament\Tables\Columns\NameTextColumn;
 use App\Filament\Tables\Columns\SlugTextColumn;
 use App\Filament\Tables\Columns\SortOrderTextColumn;
 use App\Filament\Tables\Columns\UpdatedAtTextColumn;
+use App\Filament\Tables\Filters\EnabledFilter;
+use App\Filament\Tables\ResourceTable;
 use App\Models\Technology;
 use Exception;
 use Filament\Forms\Form;
@@ -17,11 +21,15 @@ use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Support\Enums\MaxWidth;
+use Filament\Tables\Actions\BulkActionGroup;
 use Filament\Tables\Actions\CreateAction;
 use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 
 class TechnologiesRelationManager extends RelationManager
@@ -43,7 +51,7 @@ class TechnologiesRelationManager extends RelationManager
      */
     public function table(Table $table): Table
     {
-        return TechnologyResource::table($table)
+        return ResourceTable::make($table)
             ->columns([
                 NameTextColumn::make(),
                 SlugTextColumn::make(),
@@ -57,6 +65,16 @@ class TechnologiesRelationManager extends RelationManager
                 UpdatedAtTextColumn::make(),
                 DeletedAtTextColumn::make(),
             ])
+            ->filters([
+                SelectFilter::make('parent')
+                    ->relationship('parent', 'name')
+                    ->multiple()
+                    ->searchable()
+                    ->preload(),
+                EnabledFilter::make(),
+                TrashedFilter::make()
+                    ->native(false),
+            ])
             ->actions([
                 ViewAction::make()
                     ->modalWidth(MaxWidth::FiveExtraLarge),
@@ -66,6 +84,13 @@ class TechnologiesRelationManager extends RelationManager
                         fn (Technology $record): bool => $record->trashed()
                     ),
                 DeleteAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    EnableBulkAction::make(),
+                    DisableBulkAction::make(),
+                    DeleteBulkAction::make(),
+                ]),
             ])
             ->headerActions([
                 CreateAction::make()
